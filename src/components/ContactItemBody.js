67e44eaa5c7f56dbfe5ff0135a1e04da94263f31
josua-stng <josuasitanggang51@gmail.com>
2022-07-22 20:29:49 +0700

    add file

---
+++ src/components/ContactItemBody.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ContactItemBody= ({name,tag})=>{
+    return(
+        <div className='contact-item__body'>
+            <h3 className='contact-item__title'>
+                {name}
+            </h3>
+            <p className='contact-item__username'>
+                {tag}
+            </p>
+        </div>
+    )
+}
+
+export {ContactItemBody}
---
+++ src/utlis/data.js
@@ -0,0 +1,25 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: 'Dimas Saputra',
+            tag: 'dimasmds',
+            imageUrl: 'https://firebasestorage.googleapis.com/v0/b/fir-storage-70290.appspot.com/o/dimasmds.jpeg?alt=media&token=b251337f-3d57-4302-82d5-ad842e89923e',
+        },
+        {
+            id: 2,
+            name: 'Arif Faizin',
+            tag: 'arifaizin',
+            imageUrl: 'https://firebasestorage.googleapis.com/v0/b/fir-storage-70290.appspot.com/o/arifaizin.jpeg?alt=media&token=79885b8f-8b29-45bc-acb4-3fdc2534891a',
+        },
+        {
+            id: 3,
+            name: 'Rahmat Fajri',
+            tag: 'rfajri27',
+            imageUrl: 'https://firebasestorage.googleapis.com/v0/b/fir-storage-70290.appspot.com/o/rfajri27.jpeg?alt=media&token=808dd648-e171-4c98-933a-72c786fe4b63',
+        }
+    ]
+
+}
+
+export { getData }; 
